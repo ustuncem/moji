776f9cfba4265d65b4f631bed53a585fca1e984d
ustuncem <ustuncem7@gmail.com>
2023-06-19 02:14:50 +0300

    menu v1

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,9 @@
 import {useState, useCallback} from 'preact/hooks';
 
+import logo from '@assets/icon/logo.svg';
+import menu from '@assets/icon/menu.svg';
+import close from '@assets/icon/close.svg';
+
 import NavigationLink from './NavigationLink';
 
 function Navigation() {
@@ -7,11 +11,45 @@ function Navigation() {
   const toggleNavigation = useCallback(() => setShow(prev => !prev), []);
 
   return (
-    <nav className="w-full">
+    <nav className="w-full flex justify-end z-50">
+      <button onClick={toggleNavigation} className="block lg:hidden">
+        <img src={menu} alt="menu" />
+      </button>
       <ul className="text-gray-700 hidden lg:flex lg:justify-end">
         <NavigationLink path="/" placeholder="Ana Sayfa" />
         <NavigationLink path="/menu" placeholder="Menü" />
       </ul>
+      <div className={`fixed inset-0 transition-all lg:hidden ${show ? 'visible' : 'invisible'}`}>
+        <div
+          onKeyDown={toggleNavigation}
+          onClick={toggleNavigation}
+          className={`absolute inset-0 transition-colors ${
+            show ? 'bg-black/60' : 'bg-transparent'
+          }`}
+        />
+        <div
+          className={`px-4 py-[52px] w-[80%] bg-white h-full relative transition-transform delay-100 ${
+            show ? 'translate-x-0' : '-translate-x-full'
+          }`}
+        >
+          <div className="mb-12">
+            <img src={logo} alt="logo" className="w-[180px] lg:w-[260px]" />
+          </div>
+          <ul className="text-gray-700 absolute top-1/2 -translate-y-1/2">
+            <NavigationLink path="/" placeholder="Ana Sayfa" />
+            <NavigationLink path="/menu" placeholder="Menü" />
+          </ul>
+          <button
+            onClick={toggleNavigation}
+            type="button"
+            className={`grid place-items-center absolute right-0 translate-x-full top-[52px] h-[62px] aspect-square bg-primary rounded-r-2xl transition-transform delay-100 ${
+              show ? 'scale-100' : 'scale-0'
+            }`}
+          >
+            <img src={close} alt="Close Menu" className="mr-1" />
+          </button>
+        </div>
+      </div>
     </nav>
   );
 }
--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 function NavigationLink({path, placeholder}: Props) {
   return (
-    <li className="nav-link px-4 last:pr-0 font-semibold text-primary relative">
+    <li className="nav-link px-4 mb-4 last:mb-0 last:pr-0 font-semibold text-primary-800 relative">
       <a href={path}>
         <span className="nav-placeholder uppercase">{placeholder}</span>
       </a>
