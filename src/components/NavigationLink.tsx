38acc65888c6c0c09a361793ed60c462f7e63a6e
ustuncem <ustuncem7@gmail.com>
2023-06-19 18:44:07 +0300

    homepage done

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 function NavigationLink({path, placeholder}: Props) {
   return (
-    <li className="nav-link px-4 mb-4 last:mb-0 last:pr-0 font-semibold text-primary-800 relative">
+    <li className="nav-link px-4 mb-4 last:mb-0 last:pr-0 font-semibold text-primary-800 relative text-xl lg:text-lg">
       <a href={path}>
         <span className="nav-placeholder uppercase">{placeholder}</span>
       </a>
