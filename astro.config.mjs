12623fc17890e34da95f5fb9b5b8db6abcb600e1
ustuncem <ustuncem7@gmail.com>
2023-06-24 22:25:41 +0300

    mdx and menu warm up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import tailwind from '@astrojs/tailwind';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    preact(),
-    tailwind({
-      config: {applyBaseStyles: false},
-    }),
-  ],
+  integrations: [preact(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx()]
 });
